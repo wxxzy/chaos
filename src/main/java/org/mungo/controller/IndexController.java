5d9178681051be2375eb819905f8cddda20bef52
wangxingxiang <wangxingxiang@111.com.cn>
2020-01-16 10:31:48 +0800

    delete

--- src/main/java/org/mungo/controller/IndexController.java
+++ src/main/java/org/mungo/controller/IndexController.java
@@ -1,8 +1,5 @@
 package org.mungo.controller;
 
-import org.mungo.entity.BisBatchImportInfo;
-import org.mungo.service.BisBatchImportInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,15 +11,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/index")
 public class IndexController {
-    @Autowired
-    private BisBatchImportInfoService bisBatchImportInfoService;
 
     @RequestMapping("/")
     public ModelAndView getIndex(){
         ModelAndView mav = new ModelAndView("index");
 
-        //BisBatchImportInfo info = bisBatchImportInfoService.getbyId("402819814fdfc49d014fe0067fdf03b0");
-        //mav.addObject("info",info);
         return mav;
     }
 
